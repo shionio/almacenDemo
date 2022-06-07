7cec50c8fc7aef90950cc2de4037224627c2385d
Angel Gutierrez <angelgutierrez1983@gmail.com>
2022-06-07 15:19:43 -0400

    agregada el alerta de los materiles bajos de stock

--- resources/views/almacen/listaAlmacenes.blade.php
+++ resources/views/almacen/listaAlmacenes.blade.php
@@ -66,7 +66,7 @@
                     </a>
                   </abbr>
                   </td>
-                  <td></td>
+
                 </tr>
               </tbody>
             @endforeach
--- resources/views/articulos/listaArticulos.blade.php
+++ resources/views/articulos/listaArticulos.blade.php
@@ -25,35 +25,60 @@
               <th>Stock</th>
               <th>Unidad de Medida</th>
               <th>Almacen</th>
-              @if(session('rol')==0)
-              <th>Acciones</th>
+              @if(session('rol')==1)
+                <th>Acciones</th>
               @endif
             </tr>
           </thead>
             @foreach($articulos as $articulo)
-              <tbody>
+              @if($articulo->stock < 10)
+                <tbody>
+                  <tr style="background-color: orange ;">
+                    <td>{{$articulo->id_material}}</td>
+                    <td>{{$articulo->nombre_material}}</td>
+                    <td>{{$articulo->descripcion_material}}</td>
+                    <td>{{$articulo->stock}}</td>
+                    <td>{{$articulo->unidad_medida}}</td>
+                    <td>{{$articulo->descripcion_almacen}}</td>
+                    @if(session('rol')==1)
+                      <td>
+                        <a href="">
+                          <i class="fas fa-edit" style="margin-right: 5px;"></i>
+                        </a>
+                        <a href="">
+                          <i class="fas fa-eye" style="margin-right: 5px;"></i>
+                        </a>
+
+                        <a href="">
+                          <i class="fas fa-plus-circle" style="margin-right: 5px;"></i>
+                        </a>
+                    @endif
+                  </tr>
+              @else
                 <tr>
-                  <td>{{$articulo->id_material}}</td>
-                  <td>{{$articulo->nombre_material}}</td>
-                  <td>{{$articulo->descripcion_material}}</td>
-                  <td>{{$articulo->stock}}</td>
-                  <td>{{$articulo->unidad_medida}}</td>
-                  <td>{{$articulo->descripcion_almacen}}</td>
-                  @if(session('rol')==0)
-                  <td>
-                    <a href="">
-                      <i class="fas fa-sign-in-alt" name="Ingreso"></i>
-                    </a>
-                    <a href="">
-                      <i class="fas fa-sign-out-alt" style="margin-left:5px;"></i>
-                    </a>
-                    <a href="">
-                      <i class="fas fa-exchange" style="margin-left:5px;"></i>
-                  </a>
-                  </td>
-                  @endif
-                </tr>
-              </tbody>
+                    <td>{{$articulo->id_material}}</td>
+                    <td>{{$articulo->nombre_material}}</td>
+                    <td>{{$articulo->descripcion_material}}</td>
+                    <td>{{$articulo->stock}}</td>
+                    <td>{{$articulo->unidad_medida}}</td>
+                    <td>{{$articulo->descripcion_almacen}}</td>
+                    @if(session('rol')==1)
+                      <td>
+                        <a href="" onclick="hola()">
+                          <i class="fas fa-edit" style="margin-right: 5px;"></i>
+                        </a>
+                        <a href="">
+                          <i class="fas fa-eye" style="margin-right: 5px;"></i>
+                        </a>
+
+                        <a href="">
+                          <i class="fas fa-plus-circle" style="margin-right: 5px;"></i>
+                        </a>
+                      </td>
+                    @endif
+                  </tr>
+                </tbody>
+              @endif
             @endforeach
           <tfoot>
             <tr>
@@ -63,7 +88,7 @@
               <th>Stock</th>
               <th>Unidad de Medida</th>
               <th>Almacen</th>
-              @if(session('rol')==0)
+              @if(session('rol')==1)
               <th>Acciones</th>
               @endif
             </tr>
@@ -73,4 +98,9 @@
       <!-- /.card-body -->
     </div>
     <!-- /.card -->
+    <script type="text/javascript">
+      function hola(){
+        alert('xD')
+      }
+    </script>
     @endsection
